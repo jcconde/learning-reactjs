bb2faaa080bcd2c1a3773abd884a091a69d72d57
Juanca <juancarlosc@onetree.com>
2025-10-27 16:55:00 -0400

    Update main.ts to include async/await example, replacing previous imports

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,11 @@ import './style.css'
 // import './bases/03-interfaces';
 // import './bases/04-arrays';
 // import './bases/05-functions';
-import './bases/07-array-destructuring';
-
+// import './bases/07-task-destructuring';
+// import './bases/08-task-imp-exp';
+// import './bases/09-promises';
+// import './bases/10-fetch-api';
+import './bases/11-async-await';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
